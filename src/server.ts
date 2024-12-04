4f410f47f20f3d6790f1250c60c02ff147a76bfc
FewPz <sharpkung4826@gmail.com>
2024-12-04 12:47:29 +0700

    initialize TypeScript project with Express server and basic configuration

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+const port: number = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send({
+    message: 'Hello World!',
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
